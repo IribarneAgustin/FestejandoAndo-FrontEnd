bcc3fbb32b71bbbbc84b6fc1c6384622cff98d28
Agustin Iribarne <airibarne@luminaamericas.com>
2023-08-02 22:31:38 -0300

    basic auth and route

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,18 @@
 import '../Assets/Styles/App.css';
-import React, { useState, useEffect } from 'react';
-import AdministratorPanel from '././AdministratorPanel';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdministratorPanel from './AdministratorPanel';
 import WebSite from './WebSite';
 
 function App() {
-  return <AdministratorPanel />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin" element={<AdministratorPanel />} />
+        <Route path="/" element={<WebSite />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
