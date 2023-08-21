9b1d3dcd7ffbab34c74e5074a3b864c7a0b8ae49
Matias <matiasnfigueroa98@gmail.com>
2023-08-21 13:21:24 -0300

    letter missed

--- src/Components/WebSite/Layout.js
+++ src/Components/WebSite/Layout.js
@@ -1,15 +1,16 @@
-import '../../Assets/Styles/WebSite/WebSite.css'
+import '../../Assets/Styles/WebSite/WebSite.css';
 
 const Layout = ({ children }) => {
-    return (
-
-        <>
-            {children}
-            <footer>
-                <p>&copy; {new Date().getFullYear()} FestejandoAndo. Todos los derechos reservados</p>
-            </footer>
-        </>
-    );
+  return (
+    <>
+      {children}
+      <footer>
+        <p>
+          &copy; {new Date().getFullYear()} FestejandoAndo. Todos los derechos reservados
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
