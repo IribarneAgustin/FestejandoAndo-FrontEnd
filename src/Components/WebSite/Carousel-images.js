c6d3be9ee2c026931151552e8f78b3ed23f64e1a
Agustin Iribarne <airibarne@luminaamericas.com>
2023-09-20 10:24:40 -0300

    carousel responsive correction

--- src/Components/WebSite/Carousel-images.js
+++ src/Components/WebSite/Carousel-images.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../../Assets/Styles/WebSite/WebSite.css';
 import { Carousel } from 'react-responsive-carousel';
-import logo from '../../Assets/Styles/Images/BANNER.png';
+import imageCellphoneView from '../../Assets/Styles/Images/main-banner.png';
+import imagePcView from '../../Assets/Styles/Images/BANNER.png';
 
 function CarouselImages() {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
   return (
     <div className='carousel-container'>
       <Carousel
@@ -20,9 +38,15 @@ function CarouselImages() {
         showIndicators={true}
         dynamicHeight
       >
-        <div>
-          <img src={logo} alt='logo1' sizes='auto'></img>
-        </div>
+        {isMobile ? (
+          <div>
+            <img src={imageCellphoneView} alt='logo2' sizes='auto'></img>
+          </div>
+        ) : (
+          <div>
+            <img src={imagePcView} alt='logo1' sizes='auto'></img>
+          </div>
+        )}
       </Carousel>
     </div>
   );
