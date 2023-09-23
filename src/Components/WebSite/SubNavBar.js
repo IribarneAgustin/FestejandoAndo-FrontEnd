95f9c0d3d34f5c8efa4f55d443cc6148ab301c26
Agustin Iribarne <airibarne@luminaamericas.com>
2023-09-23 19:18:55 -0300

    _headers file

--- src/Components/WebSite/SubNavBar.js
+++ src/Components/WebSite/SubNavBar.js
@@ -3,21 +3,17 @@ import { Link } from 'react-router-dom';
 import '../../Assets/Styles/WebSite/WebSite.css';
 import '../../Assets/Styles/WebSite/Navbar.css';
 import ShoppingCart from './ShoppingCart';
-import { useNavigate } from 'react-router-dom';
 
 function SubNavBar() {
   const [clicked] = useState(false);
-  const navigate = useNavigate();
 
-  const home = () => {
-    navigate('/');
-  };
+
 
   return (
     <>
       <div className='container-sub-navbar'>
-        <Link className='logo'>
-          <img onClick={home} src='/logo.png' alt='logo'></img>
+        <Link to='/' className='logo'>
+          <img src='/logo.png' alt='logo'></img>
         </Link>
         <nav className={`nav ${clicked ? 'active' : ''}`}>
           <Link to='/' className='nav-link'>
