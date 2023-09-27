01a8e25c3a2582a3c944bba001401d7c15b71524
Agustin Iribarne <airibarne@luminaamericas.com>
2023-09-26 21:41:28 -0300

    Updates

--- src/Components/Loading/LoadingSpinner.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../../Assets/Styles/loadingSpinner.css';
-
-const LoadingSpinner = () => {
-  return <div className='loading-spinner'></div>;
-};
-
-export default LoadingSpinner;
--- src/Components/WebSite/Redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import shoppingReducer from './ShoppingReducer';
-
-const store = configureStore({
-  reducer: shoppingReducer,
-});
-
-export default store;
